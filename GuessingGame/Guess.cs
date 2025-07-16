49e67d0926b97306290df1734ecfa846c6908fef
Inderpreeet <A00291274@mycambrian.ca>
2025-07-15 21:11:41 -0400

    Initial commit

---
+++ GuessingGame/Guess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Guess
+{
+    public int UserGuess { get; }
+    public DateTime GuessTime { get; }
+
+    public Guess(int userGuess)
+    {
+        UserGuess = userGuess;
+        GuessTime = DateTime.Now;
+    }
+}
---
+++ GuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int answer = rnd.Next(1, 101);
+        List<Guess> guesses = new List<Guess>();
+        int userGuess = 0;
+
+        Console.WriteLine("Guess a number between 1 and 100:");
+
+        do
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine(); // Removed duplicate declaration
+
+            if (string.IsNullOrEmpty(input) || !int.TryParse(input, out userGuess))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                continue;
+            }
+
+            int index = guesses.FindIndex(g => g.UserGuess == userGuess);
+            if (index != -1)
+            {
+                int turnsAgo = guesses.Count - index;
+                Console.WriteLine($"You guessed this number {turnsAgo} turn(s) ago!");
+            }
+
+            guesses.Add(new Guess(userGuess));
+
+            if (userGuess < answer)
+                Console.WriteLine("Too low, try again.");
+            else if (userGuess > answer)
+                Console.WriteLine("Too high, try again.");
+
+        } while (userGuess != answer);
+
+        Console.WriteLine($"You Won! The answer was {answer}.");
+    }
+}
